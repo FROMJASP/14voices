25ebf90aadc74307cc9699e1e50527a949ece545
FROMJASP <42842186+FROMJASP@users.noreply.github.com>
2025-08-28 02:35:04 +0200

    fix: add NODE_OPTIONS for importMap generation and update docs
    - Fix payload:generate:importmap script with tsx module loader
    - Update CLAUDE.md with concise importMap documentation
    - Regenerate importMap after fixing module resolution

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,31 +1,91 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import { UserInfoCell as UserInfoCell_2498c455506837329098564ecaa127bb } from '../../../components/admin/cells/UserInfoCell'
+import { UserAvatarCell as UserAvatarCell_06bfa2a3d294dfb0672ca1bc540605dd } from '../../../components/admin/cells/UserAvatarCell'
+import { UserStatusCell as UserStatusCell_d564d781689a1acdfcdc133ddfd7dec7 } from '../../../components/admin/cells/UserStatusCell'
+import { UserRoleCell as UserRoleCell_bfb37b9adc5f4cb03732fb342b11d01d } from '../../../components/admin/cells/UserRoleCell'
+import { UserLastSeenCell as UserLastSeenCell_b5d142be42099c385752cb29940d2ee5 } from '../../../components/admin/cells/UserLastSeenCell'
+import { NameCell as NameCell_9c0639e919331889896aa9ea1c557661 } from '../../../components/admin/cells/NameCell'
+import { StyleTagsCell as StyleTagsCell_789abce5a437648d513c3e44bd81ed9c } from '../../../components/admin/cells/StyleTagsCell'
+import { AudioDemoCell as AudioDemoCell_c5277311b993037e42bbdbf8c691c657 } from '../../../components/admin/cells/AudioDemoCell'
+import { StatusCell as StatusCell_9552c302193d4c39b8e8f39758f33f86 } from '../../../components/admin/cells/StatusCell'
+import { CohortCell as CohortCell_c5ee63c454ad322318b0c5b17d768ea7 } from '../../../components/admin/cells/CohortCell'
+import { AvailabilityCell as AvailabilityCell_f18f4f9ba3a684cf14cb7d599fed8813 } from '../../../components/admin/cells/AvailabilityCell'
+import { RscEntryLexicalCell as RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { RscEntryLexicalField as RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { LexicalDiffComponent as LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { BlocksFeatureClient as BlocksFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { InlineToolbarFeatureClient as InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { HorizontalRuleFeatureClient as HorizontalRuleFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { UploadFeatureClient as UploadFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { BlockquoteFeatureClient as BlockquoteFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { RelationshipFeatureClient as RelationshipFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { LinkFeatureClient as LinkFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ChecklistFeatureClient as ChecklistFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { OrderedListFeatureClient as OrderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { UnorderedListFeatureClient as UnorderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { IndentFeatureClient as IndentFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { AlignFeatureClient as AlignFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { HeadingFeatureClient as HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ParagraphFeatureClient as ParagraphFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { InlineCodeFeatureClient as InlineCodeFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { SuperscriptFeatureClient as SuperscriptFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { SubscriptFeatureClient as SubscriptFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { StrikethroughFeatureClient as StrikethroughFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { UnderlineFeatureClient as UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { BoldFeatureClient as BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { default as default_26ff0c3c8c64756354667624f7216f27 } from '@/components/admin/EmailPreview'
+import { SecurityEventTypeCell as SecurityEventTypeCell_7d889b1ff63bec18bb82b7d6911cf087 } from '../../../components/admin/cells/SecurityEventTypeCell'
+import { SeverityCell as SeverityCell_64ee1493040fd92be0ee14a8c149537a } from '../../../components/admin/cells/SeverityCell'
+import { default as default_0e63ca13049e5fd88da5304cfad30e1e } from '@/components/admin/EmailAnalytics'
+import { FAQManageField as FAQManageField_c31ab92e894b3afef28b224c7ea21b03 } from '@/components/admin/ui/FAQManageField'
+import { MaintenanceModePreview as MaintenanceModePreview_ce87e2b6034154f861907fa106f8514e } from '../../../components/admin/MaintenanceModePreview'
+import { default as default_16e62c7e42dfe7700742ba3a13bf8900 } from '../../../components/admin/graphics/Icon'
+import { default as default_13338d86bf8cb9661b50b401726320cd } from '../../../components/admin/graphics/Logo'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
-  "./components/admin/BeforeLogin#default": () => import('../../../components/admin/BeforeLogin').then(mod => ({ default: mod.default })),
-  "./components/admin/AdminEnhancements#default": () => import('../../../components/admin/AdminEnhancements').then(mod => ({ default: mod.default })),
-  "./components/admin/AdminActions#default": () => import('../../../components/admin/AdminActions').then(mod => ({ default: mod.default })),
-  "./components/admin/graphics/Logo#default": () => import('../../../components/admin/graphics/Logo').then(mod => ({ default: mod.default })),
-  "./components/admin/graphics/Icon#default": () => import('../../../components/admin/graphics/Icon').then(mod => ({ default: mod.default })),
-  "./components/admin/AccountWrapper#default": () => import('../../../components/admin/AccountWrapper').then(mod => ({ default: mod.default })),
-  "./components/admin/Root#default": () => import('../../../components/admin/Root').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/ProfilePhotoCell#default": () => import('../../../components/admin/cells/ProfilePhotoCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/NameCell#default": () => import('../../../components/admin/cells/NameCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/StyleTagsCell#default": () => import('../../../components/admin/cells/StyleTagsCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/CohortCell#default": () => import('../../../components/admin/cells/CohortCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/AudioDemoCell#default": () => import('../../../components/admin/cells/AudioDemoCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/AvailabilityCell#default": () => import('../../../components/admin/cells/AvailabilityCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/StatusCell#default": () => import('../../../components/admin/cells/StatusCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/SeverityCell#default": () => import('../../../components/admin/cells/SeverityCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/SecurityEventTypeCell#default": () => import('../../../components/admin/cells/SecurityEventTypeCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/UserAvatarCell#default": () => import('../../../components/admin/cells/UserAvatarCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/UserInfoCell#default": () => import('../../../components/admin/cells/UserInfoCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/UserStatusCell#default": () => import('../../../components/admin/cells/UserStatusCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/UserRoleCell#default": () => import('../../../components/admin/cells/UserRoleCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/UserLastSeenCell#default": () => import('../../../components/admin/cells/UserLastSeenCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/VoiceoverNameCell#default": () => import('../../../components/admin/cells/VoiceoverNameCell').then(mod => ({ default: mod.default })),
-  "./components/admin/cells/VoiceoverDemosCell#default": () => import('../../../components/admin/cells/VoiceoverDemosCell').then(mod => ({ default: mod.default })),
-  "./components/admin/ui/FAQManageField#default": () => import('../../../components/admin/ui/FAQManageField').then(mod => ({ default: mod.default })),
-  "./components/admin/EmailPreview#default": () => import('../../../components/admin/EmailPreview').then(mod => ({ default: mod.default })),
-  "./components/admin/EmailAnalytics#default": () => import('../../../components/admin/EmailAnalytics').then(mod => ({ default: mod.default }))
+  "./components/admin/cells/UserInfoCell#UserInfoCell": UserInfoCell_2498c455506837329098564ecaa127bb,
+  "./components/admin/cells/UserAvatarCell#UserAvatarCell": UserAvatarCell_06bfa2a3d294dfb0672ca1bc540605dd,
+  "./components/admin/cells/UserStatusCell#UserStatusCell": UserStatusCell_d564d781689a1acdfcdc133ddfd7dec7,
+  "./components/admin/cells/UserRoleCell#UserRoleCell": UserRoleCell_bfb37b9adc5f4cb03732fb342b11d01d,
+  "./components/admin/cells/UserLastSeenCell#UserLastSeenCell": UserLastSeenCell_b5d142be42099c385752cb29940d2ee5,
+  "./components/admin/cells/NameCell#NameCell": NameCell_9c0639e919331889896aa9ea1c557661,
+  "./components/admin/cells/StyleTagsCell#StyleTagsCell": StyleTagsCell_789abce5a437648d513c3e44bd81ed9c,
+  "./components/admin/cells/AudioDemoCell#AudioDemoCell": AudioDemoCell_c5277311b993037e42bbdbf8c691c657,
+  "./components/admin/cells/StatusCell#StatusCell": StatusCell_9552c302193d4c39b8e8f39758f33f86,
+  "./components/admin/cells/CohortCell#CohortCell": CohortCell_c5ee63c454ad322318b0c5b17d768ea7,
+  "./components/admin/cells/AvailabilityCell#AvailabilityCell": AvailabilityCell_f18f4f9ba3a684cf14cb7d599fed8813,
+  "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/rsc#RscEntryLexicalField": RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/rsc#LexicalDiffComponent": LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/client#BlocksFeatureClient": BlocksFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#InlineToolbarFeatureClient": InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#HorizontalRuleFeatureClient": HorizontalRuleFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#UploadFeatureClient": UploadFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#BlockquoteFeatureClient": BlockquoteFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#RelationshipFeatureClient": RelationshipFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#LinkFeatureClient": LinkFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ChecklistFeatureClient": ChecklistFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#OrderedListFeatureClient": OrderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#UnorderedListFeatureClient": UnorderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#IndentFeatureClient": IndentFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#AlignFeatureClient": AlignFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#HeadingFeatureClient": HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ParagraphFeatureClient": ParagraphFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#InlineCodeFeatureClient": InlineCodeFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#SuperscriptFeatureClient": SuperscriptFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#SubscriptFeatureClient": SubscriptFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#StrikethroughFeatureClient": StrikethroughFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#UnderlineFeatureClient": UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#BoldFeatureClient": BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@/components/admin/EmailPreview#default": default_26ff0c3c8c64756354667624f7216f27,
+  "./components/admin/cells/SecurityEventTypeCell#SecurityEventTypeCell": SecurityEventTypeCell_7d889b1ff63bec18bb82b7d6911cf087,
+  "./components/admin/cells/SeverityCell#SeverityCell": SeverityCell_64ee1493040fd92be0ee14a8c149537a,
+  "@/components/admin/EmailAnalytics#default": default_0e63ca13049e5fd88da5304cfad30e1e,
+  "@/components/admin/ui/FAQManageField#FAQManageField": FAQManageField_c31ab92e894b3afef28b224c7ea21b03,
+  "./components/admin/MaintenanceModePreview#MaintenanceModePreview": MaintenanceModePreview_ce87e2b6034154f861907fa106f8514e,
+  "./components/admin/graphics/Icon#default": default_16e62c7e42dfe7700742ba3a13bf8900,
+  "./components/admin/graphics/Logo#default": default_13338d86bf8cb9661b50b401726320cd,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
